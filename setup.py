1152b7833d77dda2340c2826cbbcf857994c5667
Jacob van der Leeuw <jvanderleeuw@email.arizona.edu>
2020-05-26 16:53:43 -0700

    updated description and long_description within setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,10 @@ setuptools.setup(
     version="0.0.1",
     author="Jacob van der Leeuw",
     author_email="jvanderleeuw@email.arizona.edu",
-    description="Installable package for entrypoint and environment transformers",
-    long_description="Package containing entrypoint.py from entrypoint transformer and transformer_class.py "
-                     "from environment transformer",
+    description="Installable package for entrypoint and environment code within a transformer",
+    long_description="Package containing entrypoint.py from entrypoint code and transformer_class.py "
+                     "from environment code. This installable package allows for transformers to have"
+                     "common entrypoint and environment code in an installable package",
     long_description_content_type="text/markdown",
     url="https://github.com/AgPipeline/agpypeline",
     packages=setuptools.find_packages(),
