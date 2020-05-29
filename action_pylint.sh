c77c12c65b77013d774e174f30095366eb62dc18
Jacob van der Leeuw <jvanderleeuw@email.arizona.edu>
2020-05-29 11:02:48 -0700

    cleaned action_pylint.sh file

--- action_pylint.sh
+++ action_pylint.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 id=$(docker create agpypeline/agpyp:0.9.1) && docker cp $id:/code/ ./ && docker rm $id
-mv ./code/SuperBuild/ ./
-mv ./code/docker/ ./
-mv ./code/modules/ ./
-mv ./code/opendm/ ./
-mv ./code/stages/ ./
 cp ./actions_github/* ./
