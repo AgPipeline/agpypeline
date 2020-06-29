10588d2b4835ac5e36f7ade3ed847434437b20b2
Jacob van der Leeuw <jvanderleeuw@email.arizona.edu>
2020-06-29 15:12:16 -0700

    t

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="agp",
-    version="0.0.5",
+    version="0.0.12",
     author="Jacob van der Leeuw",
     author_email="jvanderleeuw@email.arizona.edu",
     description="Installable package for entrypoint and drone-specific environment code within a transformer",
