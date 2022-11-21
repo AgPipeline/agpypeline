b9de70292dfa463896287f733e956cdd902cd18e
Chris Schnaufer <schnaufer@email.arizona.edu>
2022-11-21 12:48:54 -0700

    Changes based on testing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="agpypeline",
-    version="0.0.50",
+    version="0.0.51",
     author="Jacob van der Leeuw",
     author_email="jvanderleeuw@email.arizona.edu",
     description="Installable package for entrypoint and drone-specific environment code within a transformer",
